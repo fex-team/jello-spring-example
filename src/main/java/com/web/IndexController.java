f298ce1ac1ff4891add83cef2f53939f26101b4a
liaoxuezhi <liaoxuezhi@baidu.com>
2014-08-22 16:42:18 +0800

    init

---
+++ src/main/java/com/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        return "home/page/index.vm";
+    }
+}
